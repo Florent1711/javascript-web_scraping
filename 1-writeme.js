43a6d92c9822c67cde183639091dff4bb50eb3b3
Florent1711 <graphit01@hotmail.com>
2024-05-14 13:16:46 +0200

    modif de 1-writeme.js

--- 1-writeme.js
+++ 1-writeme.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, { encoding: 'utf-8' }, (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`Content written to ${filePath}`);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
