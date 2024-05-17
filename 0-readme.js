da916a9bf0b5ae3302a06e9a7864de3ad579ad91
Florent1711 <graphit01@hotmail.com>
2024-05-17 17:08:10 +0200

    modif de 0-readme.js

--- 0-readme.js
+++ 0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+const sourcePath1 = process.argv[2];
+
+fs.readFile(sourcePath1, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
